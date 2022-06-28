db6edf661f746f1dec47a9fe2102ceea361b8ac3
Mylena Vitoriano <71454293+mylenavitoriano@users.noreply.github.com>
2022-06-27 21:46:44 -0300

    realizado layout do Video e iniciado processo de rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
 import { Event } from "./pages/Event"
+import { client } from './lib/apollo'
+import { ApolloProvider } from '@apollo/client'
+import { BrowserRouter } from "react-router-dom"
+import { Router } from './Router'
+
 
 function App() {
   return (
     <>
-      <Event/>
+    <ApolloProvider client={ client }>
+      <BrowserRouter>
+        <Router/>
+      </BrowserRouter>
+    </ApolloProvider>
     </>
   )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,5 +1,83 @@
+import { DefaultUi, Player, Youtube } from "@vime/react";
+import { CaretRight, DiscordLogo, FileArrowDown, Image, Lightning } from "phosphor-react";
+
+import '@vime/core/themes/default.css'
+
 export function Video(){
     return(
-        <div className="flex-1">Video</div>
+        <div className="flex-1">
+            <div className="bg-black flex justify-center">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    <Player>
+                        <Youtube videoId="SO4-izct7Mc"/>
+                        <DefaultUi />
+                    </Player>
+                </div>
+            </div>
+
+            <div className="p-8 max-w-[1100px] mx-auto">
+                <div className="flex items-start gap-16">
+                    <div className="flex-1">
+                        <h1 className="text-2xl font-bold">
+                            Aula 01 - Abertura do Ignite Lab
+                        </h1>
+                        <p className="mt-4 text-gray-200 leading-relaxed">
+                            Na aula 1 você vai entender quais são as oportunidades que você pode ter se especializando em ReactJS e porque essa é a decisão mais inteligente pra sua carreira no momento! E também vamos dar início ao projeto que vamos desenvolver nessa semana: uma plataforma de conteúdos em vídeo. Bora codar!
+                        </p>
+
+                        <div className="flex items-center gap-4 mt-6">
+                            <img src="https://avatars.githubusercontent.com/u/71454293?v=4" alt="" className="h-16 w-16 rounded-full" />
+
+                            <div className="leading-relaxed">
+                                <strong className="font-bold text-2xl">Mylena Vitoriano</strong>
+                                <span className="text-gray-200 text-sm block">Desenvolvedora Front-end</span>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col gap-4">
+                        <a href="" className="p-4 text-sm bg-green-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-green-700 transition-colors">
+                            <DiscordLogo size={24}/>
+                            Comunidade do Discord
+                        </a>
+
+                        <a href="" className="p-4 text-sm border border-blue-500 text-blue-500 flex items-center justify-center rounded font-bold uppercase gap-2 hover:bg-blue-500 hover:text-gray-900 transition-colors">
+                            <Lightning size={24}/>
+                           Acesse o desafio
+                        </a>
+                    </div>
+                </div>
+
+                <div className="gap-8 mt-20 grid grid-cols-2">
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <FileArrowDown size={40} />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Materia complementar</strong>
+                            <p className="tex-sm text-gray-200 mt-2">Acesse o material complementar para acelerar o seu desenvolvimento</p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
+                        </div>
+                    </a>
+
+                    <a href="" className="bg-gray-700 rounded overflow-hidden flex items-stretch gap-6 hover:bg-gray-600 transition-colors">
+                        <div className="bg-green-700 h-full p-6 flex items-center">
+                            <Image size={40} />
+                        </div>
+                        <div className="py-6 leading-relaxed">
+                            <strong className="text-2xl">Wallpapers exclusivos</strong>
+                            <p className="tex-sm text-gray-200 mt-2">Baixe wallpapers exclusivos do Ignite Lab e personalize a sua máquina</p>
+                        </div>
+                        <div className="h-full p-6 flex items-center">
+                            <CaretRight size={24} />
+                        </div>
+                    </a>
+                    
+                </div>
+                
+            </div>
+        </div>
     )
 }
