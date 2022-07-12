44e0d19edcd2d9d62fb2ecfe9842e829ff763c19
Mylena Vitoriano <mylenabarbosa08@gmail.com>
2022-07-12 17:20:52 -0300

    realizado layout da home e iniciado processo de cadastro

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,6 +1,9 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client"
 
 export const client = new ApolloClient({
-    uri: 'https://api-sa-east-1.graphcms.com/v2/cl4o7n7j80ygt01z70ontdn69/master',
+    uri: import.meta.env.VITE_API_URL,
+    headers: {
+        'Authorization': `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}`
+    },
     cache: new InMemoryCache()
 })
